5087dcf0456a7eda7a37840d18caefe1b0d91254
Tairo <188715239+Tairotai@users.noreply.github.com>
2026-01-27 18:58:59 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,1 +1,10 @@
-const express=require('express'),app=express();app.use(express.static('web'));app.listen(3000);
+const express = require('express');
+const app = express();
+
+app.use(express.static('web'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Servidor iniciado en el puerto " + port);
+});
