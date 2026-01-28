fd4f08e423ab7fb86ee6685eab32e5435457d716
Tairo <188715239+Tairotai@users.noreply.github.com>
2026-01-27 19:30:14 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -2,20 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Servir los archivos de la carpeta 'web'
 app.use(express.static(path.join(__dirname, 'web')));
-
 const port = process.env.PORT || 3000;
+process.env.PORT = port; 
 
 try {
-    console.log("Iniciando motor Ogar...");
-    // Simplemente llamamos al index del motor. 
-    // Él mismo leerá el process.env.PORT que Render le da.
     require('./src/index.js');
 } catch (e) {
-    console.log("Error al cargar el motor: " + e.message);
+    console.log("Error: " + e.message);
 }
 
 app.listen(port, () => {
-    console.log("Servidor Web y Ogar listos en puerto: " + port);
+    console.log("Server port: " + port);
 });
