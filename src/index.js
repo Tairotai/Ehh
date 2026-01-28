507750bcbb73352de84f61e01cccfbd116111d11
Tairo <188715239+Tairotai@users.noreply.github.com>
2026-01-27 19:30:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 var Commands = require('./modules/CommandList');
 var GameServer = require('./GameServer');
 var AsyncConsole = require('asyncconsole');
-
 var showConsole = true;
 
-console.log("[Game] Ogar - An open source Agar.io server implementation");
-
 process.argv.forEach(function(val) {
-    if (val == "--noconsole") {
-        showConsole = false;
-    } else if (val == "--help") {
-        console.log("Proper Usage: node index.js");
-        console.log("    --noconsole         Disables the console");
-        console.log("    --help              Help menu.");
-        console.log("");
-    }
+    if (val == "--noconsole") showConsole = false;
 });
 
 var gameServer;
@@ -22,24 +12,16 @@ startServer();
 
 function startServer() {
     gameServer = new GameServer();
-    
-    // IMPORTANTE: Render nos da el puerto aquí
     gameServer.config.serverPort = process.env.PORT || 3000; 
-    
     gameServer.start();
 
     gameServer.shutdownHandle = function() {
         process.exit(0);
     };
     gameServer.restartHandle = function(timeout) {
-        gameServer.restartScheduled = new Date();
-        gameServer.restartAt = new Date(Date.now() + timeout);
-        gameServer.restartId = setTimeout(function() {
+        setTimeout(function() {
             gameServer.socketServer.close();
             gameServer.httpServer.close();
-            gameServer.gameServer = null;
-            if (global.gc) global.gc(); 
-            process.stdout.write("\u001b[2J\u001b[0;0H"); 
             startServer();
         }, timeout);
     };
