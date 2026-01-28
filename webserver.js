df1d500b0886c29f3c5e8944a792f3dc6b4f36af
Tairo <188715239+Tairotai@users.noreply.github.com>
2026-01-27 19:37:34 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -3,15 +3,14 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'web')));
-const port = process.env.PORT || 3000;
-process.env.PORT = port; 
 
-try {
-    require('./src/index.js');
-} catch (e) {
-    console.log("Error: " + e.message);
-}
+const port = process.env.PORT || 3000;
+
+console.log("Cargando motor desde: ./src/index.js");
+
+// Si esto falla, Render nos dará el error real en el log
+require('./src/index.js');
 
 app.listen(port, () => {
-    console.log("Server port: " + port);
+    console.log("WEB Y MOTOR LISTOS EN PUERTO: " + port);
 });
