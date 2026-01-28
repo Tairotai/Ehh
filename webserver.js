cae19c81a7697f7d964fb58b020ad15ad473f50b
Tairo <188715239+Tairotai@users.noreply.github.com>
2026-01-27 19:01:41 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('web'));
+app.use(express.static(path.join(__dirname, 'web')));
 
 const port = process.env.PORT || 3000;
 
+try {
+    // Esto arranca el motor de Ogar (necesitas la carpeta src)
+    const Ogar = require('./src/index.js'); 
+} catch (e) {
+    console.log("Error: Falta la carpeta 'src' de Ogar en el repositorio.");
+}
+
 app.listen(port, () => {
-    console.log("Servidor iniciado en el puerto " + port);
+    console.log("Servidor y Web listos en el puerto " + port);
 });
